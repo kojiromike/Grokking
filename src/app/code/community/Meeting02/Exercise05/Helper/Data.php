5f09e05f238e860077fb8561cac1dce100d07e03
Michael A. Smith <msmith3@ebay.com>
2014-06-08 21:02:45 -0400

    Exercise 05

---
+++ src/app/code/community/Meeting02/Exercise05/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class Meeting02_Exercise05_Helper
+ */
+class Meeting02_Exercise05_Helper_Data extends Mage_Payment_Helper_Data
+{
+    protected function _construct()
+    {
+        parent::_construct();
+    }
+}
