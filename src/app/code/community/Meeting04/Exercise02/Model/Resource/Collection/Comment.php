b8f70b0041aa41104e365a65d98292d404abeafc
Michael A. Smith <msmith3@ebay.com>
2014-06-29 13:51:32 -0400

    Meeting 04 Exercise 02

---
+++ src/app/code/community/Meeting04/Exercise02/Model/Resource/Collection/Comment.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smithm5
+ * Date: 6/28/14
+ * Time: 11:51 PM
+ */
+
+class Meeting04_Exercise02_Model_Resource_Collection_Comment extends Mage_Catalog_Model_Resource_Collection_Abstract {
+
+} 
