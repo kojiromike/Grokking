ffa41c226037b0f6e0824672a9132e0e04e6f7f0
Michael A. Smith <msmith3@ebay.com>
2014-06-30 00:49:11 -0400

    Meeting 04 Exercise 03

---
+++ src/app/code/community/Meeting04/Exercise03/Model/Comment.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smithm5
+ * Date: 6/28/14
+ * Time: 11:32 PM
+ */
+
+/**
+ * Class Meeting04_Exercise03_Model_Comment
+ *
+ * @method Meeting04_Exercise03_Model_Comment setId($id)
+ * @method mixed getId()
+ * @method Meeting04_Exercise03_Model_Comment setText(string $text)
+ * @method string getText()
+ */
+class Meeting04_Exercise03_Model_Comment extends Mage_Core_Model_Abstract {
+    protected function _construct()
+    {
+        $this->_init('meeting0403/comment');
+    }
+}
---
+++ src/app/code/community/Meeting04/Exercise03/Model/Resource/Comment/Collection.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smithm5
+ * Date: 6/28/14
+ * Time: 11:51 PM
+ */
+
+class Meeting04_Exercise03_Model_Resource_Comment_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+    /**
+     * Item object class name
+     *
+     * @var string
+     */
+    protected $_itemObjectClass = 'Meeting04_Exercise03_Model_Comment';
+}
+
